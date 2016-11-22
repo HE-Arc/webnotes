fcb45969824fef5870a830c782d09bd408c12b83
Daniel Rodrigues Lourenço <daniel.24.rodrigues@gmail.com>
2016-11-22 10:38:57 +0100

    Add all migrations files

--- database/migrations/2016_11_22_093541_create_groups_table.php
+++ database/migrations/2016_11_22_093541_create_groups_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGroupsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('groups', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email', 250)->unique();
-            $table->string('password');
-            $table->string('avatar');
-            $table->rememberToken();
+            $table->string('name', 250)->unique();
+            $table->string('description');
+            $table->string('icon');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('groups');
     }
 }
